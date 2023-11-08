3e7905d760b57a10d25e5d4532aa85cf9e890421
Johnny Kantaros <jkantaros@middlebury.edu>
2023-11-07 19:52:26 -0500

    fixed merge pt2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,9 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-<<<<<<< HEAD
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
-=======
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
->>>>>>> 1070d6fb40ffe82fff291478055a0ddc51d3ee0c
   ],
   theme: {
     extend: {
