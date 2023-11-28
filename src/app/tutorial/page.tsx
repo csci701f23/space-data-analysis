e50af7b47bbdc593a612b20ea167bbe456fa8ca1
lilakosowsky <lkosowsky@middlebury.edu>
2023-11-28 11:24:12 -0500

    started work on tutorial page

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -6,6 +6,10 @@ export default function Page() {
   return (
     <main className={styles.main}>
         <Link href={"/"}>Back to home</Link>
+        <div className='m-5'>
+          <h1 className='text-2xl'>Welcome to the Mittelman Observtory Image Calibration Site!</h1>
+          <h1 className='text-xl'>Here you will find all the steps necessary to calibrate your science images.</h1>
+        </div>
     </main>
   )
 }
